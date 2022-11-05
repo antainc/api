0422eb8407924407b3add6551349ee1e51fdf293
x86wvrld <80956296+wvrld@users.noreply.github.com>
2022-11-05 22:39:38 +0100

    Update cmc-circulating-supply.ts

--- api/cmc-circulating-supply.ts
+++ api/cmc-circulating-supply.ts
@@ -1,9 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getCirculatingSupply } from './_utils';
 
-const vestingContracts = [
-];
-
 const unissuedHolders = [
   "", // DAO vesting
   "", // Team
@@ -16,6 +13,6 @@ export default async function handler(
   _: NextApiRequest,
   response: NextApiResponse,
 ) {
-  const circulatingSupply = await getCirculatingSupply(vestingContracts, unissuedHolders)
+  const circulatingSupply = await getCirculatingSupply(unissuedHolders)
   response.status(200).send(circulatingSupply.toFixed(8));
 }
